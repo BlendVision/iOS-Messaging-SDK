af6b1d16386f290de322bba55fd37b65d7054f8d
Bing Kuo <bingkuo@kkstream.com>
2024-03-12 14:53:55 +0800

    Upgrade to 1.0.0

--- Package.swift
+++ Package.swift
@@ -4,23 +4,38 @@
 import PackageDescription
 
 let package = Package(
-    name: "BlendVisionMessagingSDK",
+    name: "BVMessagingSDK",
     platforms: [
-            .iOS(.v14)
-        ],
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "BlendVisionMessagingSDK",
-            targets: ["BlendVisionMessagingSDK"]),
+            name: "BVMessagingSDK",
+            targets: ["BVMessagingSDK", "Alamofire", "CocoaMQTT", "Starscream", "MqttCocoaAsyncSocket"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "BlendVisionMessagingSDK"),
-        .testTarget(
-            name: "BlendVisionMessagingSDKTests",
-            dependencies: ["BlendVisionMessagingSDK"]),
+        .binaryTarget(
+            name: "Alamofire",
+            path: "Sources/Frameworks/Alamofire.xcframework"
+        ),
+        .binaryTarget(
+            name: "CocoaMQTT",
+            path: "Sources/Frameworks/CocoaMQTT.xcframework"
+        ),
+        .binaryTarget(
+            name: "MqttCocoaAsyncSocket",
+            path: "Sources/Frameworks/MqttCocoaAsyncSocket.xcframework"
+        ),
+        .binaryTarget(
+            name: "Starscream",
+            path: "Sources/Frameworks/Starscream.xcframework"
+        ),
+        .binaryTarget(
+            name: "BVMessagingSDK",
+            path: "Sources/Frameworks/BVMessagingSDK.xcframework"
+        )
     ]
 )
