19414588d8ab8ad68da35df0e7b8210947e51816
Bing Kuo <bingkuo@kkstream.com>
2024-03-12 14:58:23 +0800

    chore: Add dependencies

--- Package.swift
+++ Package.swift
@@ -21,6 +21,22 @@ let package = Package(
             name: "BlendVisionMessagingSDK",
             url: "https://drive.google.com/file/d/1YpHjKF5aeuAfCt2DhAN1ew9TYyURak-h/view?usp=drive_link",
             checksum: "8cb8fe997944c73d016afb0d98e893fec90f3283853eeff39c051ccf160bda47"
+        ),
+        .binaryTarget(
+            name: "Alamofire",
+            path: "Sources/Frameworks/Alamofire.xcframework"
+        ),
+        .binaryTarget(
+            name: "CocoaMQTT",
+            path: "Sources/Frameworks/CocoaMQTT.xcframework"
+        ),
+        .binaryTarget(
+            name: "MqttCocoaAsyncSocket",
+            path: "Sources/Frameworks/MqttCocoaAsyncSocket.xcframework"
+        ),
+        .binaryTarget(
+            name: "Starscream",
+            path: "Sources/Frameworks/Starscream.xcframework"
         )
     ]
 )
