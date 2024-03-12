66ab1f7625c0f4d2d4a4f9e8843805b2cf22d5a1
Bing Kuo <bingkuo@kkstream.com>
2024-03-12 17:12:40 +0800

    chore: Modified Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BVMessagingSDK",
-            targets: ["BVMessagingSDK", "Alamofire", "CocoaMQTT", "Starscream"]),
+            targets: ["BVMessagingSDK", "Alamofire", "CocoaMQTT", "Starscream", "MqttCocoaAsyncSocket"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
