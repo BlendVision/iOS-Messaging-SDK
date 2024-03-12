bd22f9b8f9f6911f8f49b2c00a6db8fdbad9ebb6
Bing Kuo <bingkuo@kkstream.com>
2024-03-12 15:52:10 +0800

    chore: Modified Package.swift

--- Package.swift
+++ Package.swift
@@ -17,9 +17,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "BlendVisionMessagingSDK"
-        ),
         .binaryTarget(
             name: "Alamofire",
             path: "Sources/Frameworks/Alamofire.xcframework"
@@ -37,11 +34,9 @@ let package = Package(
             path: "Sources/Frameworks/Starscream.xcframework"
         ),
         .binaryTarget(
-            name: "BVMessagingSDK",
-            path: "Sources/Frameworks/BVMessagingSDK.xcframework"
-        ),
-        .testTarget(
-            name: "BlendVisionMessagingSDKTests",
-            dependencies: ["BlendVisionMessagingSDK"]),
+            name: "BlendVisionMessagingSDK",
+            url: "https://github.com/bing-kuo/MessagingSDKDemo/releases/download/0.0.1/BVMessagingSDK.xcframework.zip",
+            checksum: "8cb8fe997944c73d016afb0d98e893fec90f3283853eeff39c051ccf160bda47"
+        )
     ]
 )
